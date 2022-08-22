0f25a06416704a511cc887ab2cf9aa98d0c50747
SONALSINGH45 <99395129+SONALSINGH45@users.noreply.github.com>
2022-08-22 21:13:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,109 +1,120 @@
-"use strict";
+
+// const express = require("express");
+// const mysql = require('mysql');
+// var app = express();
+// const bodyparser = require("body-parser");
+// //const res = require("express/lib/response");
+// //const res = require("express/lib/response");
+// app.use(bodyparser.json);
+// app.use(express.json());
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "books",
+//     multipleStatements: true
+// });
+// con.connect(function (err) {
+//     if (err)
+//         throw err;
+//     console.log("Connected!");
+// });
+
+// app.listen(8000, (req,res) => {
+//     console.log("runinggggg");
+// });
+
+// app.get('/',(req,res)=>{
+//     console.log('i am called');
+// })
+// //get
+// app.get('/book', (_req, _res) => {
+//     console.log("vgvg");
+//     con.query('SELECT * FROM authors', (err, _rows, _fields) => {
+//         if (!err)
+//             //console.log(_rows);
+//             res.send(_rows)
+//         else
+//             console.log(err);
+//     });
+// });
+// //get by id
+// app.get('/book/:id', (_req, _res) => {
+//     console.log("hgsxh");
+//     con.query('SELECT * FROM authors WHERE id=?', [_req.params.id], (err, _rows, _fields) => {
+//         if (!err)
+//             _res.send(_rows);
+//         else
+//             console.log(err);
+//     });
+// });
+// /// delete BY ID 
+// app.delete('/book/:id', (_req, _res) => {
+//     con.query('DELETE FROM authors WHERE id=?', [_req.params.id], (err, _rows, _fields) => {
+//         if (!err)
+//             _res.send("DELETED");
+//         else
+//             console.log(err);
+//     });
+// });
+// //all dlt
+// app.delete("/book", (req, _res) => {
+//     let query = 'DELETE FROM authors  ';
+//     con.query(query, (err, _rows) => {
+//         if (err)
+//             throw err;
+//         console.log('Cleared  Table');
+//     });
+// });
+// //insertion
+// app.post('/insert', function (req, res, _next) {
+//     var id = req.body.id;
+//     var name = req.body.name;
+//     var email = req.body.email;
+//     var sql = `INSERT INTO authors (id, name, email) VALUES ("${id}", "${name}","${email}")`;
+//     con.query(sql, function (err, _result) {
+//         if (err)
+//             throw err;
+//         console.log('record inserted');
+//         res.redirect('/authors');
+//     });
+// });
+// //update 
+// app.put('/fetch', (req, res) => {
+//     con.query("UPDATE `authors` SET `name`=?, `email`=? where `id`=?", [req.body.name, req.body.email, req.body.id], function (err, rows, _fields) {
+//         if (err)
+//             throw err;
+//         res.end(JSON.stringify(rows));
+//     });
+// });
+
 const express = require("express");
-const students = require("ipaddr.js");
-//const createConnection = require("typeorm")
+//const mysql = require('mysql');
+
 const bodyparser = require("body-parser");
-//onst app = express()
-var mysql = require('mysql');
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "typeorm_test_db",
-    multipleStatements: true
-});DELETE
-con.connect(function (err) {
-    if (err)
-        throw err;
-    console.log("Connected!");
-});
-const app = express();
-//app.use(cors());
-app.use(express.json());
+var app = express();
 app.use(bodyparser.json());
-app.listen(7000, () => {
-    console.log("listining");
-});
-// GET 
-app.get('/students', (_req, _res) => {
-   con.query('select * from students',(err,rows,_fields)=>{
-       if(!err)
-       console.log(rows)
-       else
-       console.log(err)
-   })
-})
-///PATCH METHOD 
-app.patch('/students/:id', function (req, res) {
-    var id = req.params.id;
-    var _a = req.body, first_name = _a.first_name, last_name = _a.last_name, email = _a.email;
-    var student = students.find(function (student) { return student.id == id; });
-    if (first_name)
-        student.first_name = first_name;
-    if (last_name)
-        student.last_name = last_name;
-    if (email)
-        student.email = email;
-    res.send("changed");
-});
-// fetch by id
-app.get("/fetch/:id", (req, res) => {
-    con.query("select * from students where id = ?", [req.params.id], function (err, result, _fields) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            res.send(result);
-            var r = (JSON.parse(JSON.stringify(result)));
-            console.log(r);
-        }
-    });
-});
+app.use(express.json());
 
-//delete 
-app.delete("/students/:id", (req,_res) => {
-    con.query('DELETE from  students WHERE id = ?', [req.params.id],  (err, _result, _fields) =>{
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log(_result)
-            res.send(result)
-               var r=(JSON.parse(JSON.stringify(_result)))
-               console.log(r)
-            console.log("deleted");
-        }
-    });
-});
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     database: "books",
+//     multipleStatements: true
+// });
 
-//all dlt
-app.delete("/students", (req,_res) => {
-let query = 'DELETE FROM students';
-  
-    con.query(query, (err, rows) => {
-        if(err) throw err;
-  
-        console.log('Cleared users Table');
-    });
+// con.connect(function (err) {
+//     if (err)
+//         throw err;
+//     console.log("Connected!");
+// });
+const port = 8000
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-//insert new student
-app.post('/insert', function(req, res, next) {
-    var id  = req.body.id;
-    var first_name = req.body.first_name;
-    var last_name = req.body.last_name;
-    var email= req.body.email
-  
-    var sql = `INSERT INTO students (id, first_name, last_name, email,  created_at) VALUES ("${id}", "${first_name}", "${last_name}","${email}", NOW())`;
-    con.query(sql, function(err, result) {
-      if (err) throw err;
-      console.log('record inserted');
-      res.redirect('/students');
-    });
-  });
-app.put('/fetch', (req, res) => {
-    con.query("UPDATE `students` SET `first_name`=?,  `last_name`=?,`email`=? where `id`=?", [req.body.first_name, req.body.last_name, req.body.email, req.body.id], function (err, rows, _fields) {
-        if (err)
-            throw err;
-        res.end(JSON.stringify(rows));
-    });
-});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
